051d37ce4aa7502f9bd695f480a86725c0f13d3b
weiping <weiping@bneing.com>
2014-09-29 17:41:58 +1000

    add comments

--- traversal.py
+++ traversal.py
@@ -2,24 +2,6 @@ import sys, os
 from graph.xgraph import *
 from graph.priority_dict import *
 
-
-def bfs(G, s=None):
-    if s is None:
-        s = G.getVertex(0)
-    Q = [s] # Q stores the discovered vertices but not further explored yet 
-    while Q:
-        v = Q.pop(0)
-        yield v
-        for e in G.adjcent(v):
-            source, dest = e['source'], e['dest']
-            # if it's start vertex or already discovered or processed 
-            # avoid putting this vertex in Q repeatedly 
-            if dest == s or dest['predecessor']: 
-                continue
-            Q.append(dest) # add new to be further explored
-            dest['predecessor'] = v
-
-
 def dfs_norec(G, s=None):
     if s is None:
         s = G.getVertex(0)
@@ -45,16 +27,36 @@ def dfs(G, T, s=None):
     print 'finish %s at time %d' % (str(s), s['ftime'])
     return T
 
+def bfs(G, s=None):
+    if s is None:
+        s = G.getVertex(0)
+    Q = [s] # Q stores the discovered vertices but not further explored yet 
+    while Q:
+        v = Q.pop(0)
+        yield v
+        for e in G.adjcent(v):
+            source, dest = e['source'], e['dest']
+            # if it's start vertex or already discovered
+            # avoid putting this vertex in Q repeatedly 
+            if dest == s or dest['predecessor']: 
+                continue
+            Q.append(dest) # add new to be further explored
+            dest['predecessor'] = v
+
 def dijkstra(G, s, d):
-    """ The single source shortest path on a non-negative weighted graph """
+    """ The single source shortest path on a non-negative weighted graph 
+    @param G -- the graph
+    @param s -- the starting vertex
+    @param d -- the ending vertex
+    """
 
     # Q: key is vertex
-    #    value is accumulated cost from 's' to the vertex
+    #    value is accumulated cost from 's' to this vertex
     #    priority is the current cost of each vertex
 
     Q = priority_dict()
     Q[s] = 0.0 # the starting vertex has zero cost
-    processed = []
+    processed = [] # those have been popped (processed) from Q
     while Q:
         # extract the vertex with lowest cost so far
         # first u should be starting 's' and then
@@ -67,13 +69,13 @@ def dijkstra(G, s, d):
             # if dest is already extracted from Q continue next
             if dest in processed:
                 continue
-            # put the newly discovered vertex in the Q initializing the cost as the maximum 
+            # if dest is newly discovered vertex initializing the cost as the maximum 
             Q.setdefault(dest, float('inf'))
             cost = value + e['weight'] # cost by following the edge 'e'
             if cost < Q[dest]: 
                 # the dest vertex could be some discovered but not popped from Q yet
                 # relax the 'dest' vertex by updating the accumulated cost
-                # the newly discovered is always updated
+                # the newly discovered one is always updated
 
                 Q[dest] = cost
                 # there is a shorter path from u 
